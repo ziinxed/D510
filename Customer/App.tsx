e775858b9696868af8aee1d747e18d48d5b7ad37
deok <ziinxed@gmail.com>
2019-10-20 18:37:54 +0900

    Merge branch 'master' of https://github.com/ziinxed/D510
    # Conflicts:
    #	package.json

--- Customer/App.tsx
+++ Customer/App.tsx
@@ -1,41 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
-
-export default function PriceTextInput() {
-    const [value, onChangeText] = React.useState('0');
-
-    return (
-        <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 2, fontSize: 32 }}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-            keyboardAppearance={"default"}
-            keyboardType={"numeric"}
-        />
-    );
-}
-
-
-export default function App(){
-  return (
-    <View style={styles.container}>
-        <View style={styles.wrapper}>
-      <Text style={styles.title}>
-        가격을 입력하세요
-    </Text>
-            <PriceTextInput/>
-            <Button
-                title="수락"
-                onPress={() => Alert.alert("wow")}
-            />
+import { StyleSheet, Text, View, TextInput,  Alert} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
 
 
 
-        </View>
 
-    </View>
-  );
+export const input = React.createRef<Input>();
+
+export default class App extends React.Component
+{
+    // state 추가함.
+    state = {
+        price : 0
+    }
+
+    render()
+    {
+        return (
+            <View style={styles.container}>
+                <View style={styles.wrapper}>
+                    <Text style={styles.title}>
+                        가격을 입력하세요
+                    </Text>
+
+
+                    <Input
+                        leftIcon={
+                            <Icon
+                                name='money'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                        placeholder='price'
+                        errorStyle={{color: 'black'}}
+                        errorMessage='ENTER Price Here'
+                        keyboardAppearance={'default'}
+                        keyboardType={'numeric'}
+                        onChangeText={(price) => this.setState({price})}
+
+                    />
+                    <Button
+                        title="SUBMIT"
+                        onPress={() => Alert.alert(this.state.price)}
+                    />
+
+                </View>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
